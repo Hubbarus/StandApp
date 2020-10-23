01189239fc7d35ce56cdb9d42b37ee9c5f08ea43
Paul Ponomarev <rocksquare@gmail.com>
2020-10-23 12:03:56 +0300

    working consumer class done refreshing page every 3 sec

--- src/main/java/service/Consumer.java
+++ src/main/java/service/Consumer.java
@@ -1,79 +1,72 @@
-//package service;
-//
-//import dto.ItemDTO;
-//import org.apache.activemq.ActiveMQConnection;
-//import org.apache.activemq.ActiveMQConnectionFactory;
-//import utils.JsonDeserializer;
-//
-//import javax.ejb.EJB;
-//import javax.ejb.LocalBean;
-//import javax.ejb.MessageDriven;
-//import javax.ejb.Stateful;
-//import javax.inject.Inject;
-//import javax.jms.Connection;
-//import javax.jms.JMSException;
-//import javax.jms.Message;
-//import javax.jms.MessageConsumer;
-//import javax.jms.MessageListener;
-//import javax.jms.ObjectMessage;
-//import javax.jms.Queue;
-//import javax.jms.Session;
-//import javax.jms.TextMessage;
-//import java.util.List;
-//
-//@MessageDriven
-//public class Consumer {
-//
-//    @EJB private JsonDeserializer deserializer;
-////    @Inject ShopMessageListener listener;
-////    private ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(
-////            ActiveMQConnection.DEFAULT_USER,
-////            ActiveMQConnection.DEFAULT_PASSWORD,
-////            ActiveMQConnection.DEFAULT_BROKER_URL);
-//    @Inject private MSGConnectionFactory factory;
-//    private String queueName = "QUEUE";
-//
-//    public List<ItemDTO> receive() {
-//        try {
-//            Connection connection = factory.createQueueConnection();
-//            connection.start();
-//
-//            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-//            Queue queue = session.createQueue(queueName);
-//
-//            MessageConsumer consumer = session.createConsumer(queue);
-//            consumer.setMessageListener(new MsgListener());
-////            TextMessage receive = (TextMessage) consumer.receive();
-////            String text = receive.getText();
-////
-////            List<ItemDTO> items = deserializer.deserialize(text);
-////            System.out.println(items);
-//
-//            consumer.close();
-//            session.close();
-//            connection.close();
-//            return null;
-//        } catch (JMSException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//    private class MsgListener implements MessageListener {
-//
-//        @Override
-//        public void onMessage(Message message) {
-//            try {
-//            if (message instanceof TextMessage) {
-//                TextMessage msg = (TextMessage) message;
-//                String text = msg.getText();
-//                List<ItemDTO> deserialize = deserializer.deserialize(text);
-//                System.err.println("MEEEEEEEESAAAAAGE RECEEEEEIIIIVIED! : " + deserialize);
-//            }
-//                System.err.println("HOOORAAAY!");
-//        } catch (JMSException e) {
-////            context.setRollbackOnly();
-//        }
-//        }
-//    }
-//}
+package service;
+
+import dto.ItemDTO;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import servlet.StandServlet;
+import utils.JsonDeserializer;
+
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
+import java.util.List;
+
+@Singleton
+public class Consumer {
+
+    @EJB private JsonDeserializer deserializer;
+    @Inject private StandServlet servlet;
+
+    private ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(
+            ActiveMQConnection.DEFAULT_USER,
+            ActiveMQConnection.DEFAULT_PASSWORD,
+            ActiveMQConnection.DEFAULT_BROKER_URL);
+
+    private List<ItemDTO> items = new ArrayList<>();
+    private String queueName = "QUEUE";
+
+    public void start() {
+        try {
+            Connection connection = factory.createQueueConnection();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue queue = session.createQueue(queueName);
+
+            MessageConsumer consumer = session.createConsumer(queue);
+            consumer.setMessageListener(new ShopListener());
+            connection.start();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ShopListener implements MessageListener {
+
+        @Override
+        public void onMessage(Message message) {
+            try {
+                if (message instanceof TextMessage) {
+                    TextMessage msg = (TextMessage) message;
+                    String text = msg.getText();
+                    items = deserializer.deserialize(text);
+                }
+            } catch (JMSException e) {
+//            context.setRollbackOnly();
+            }
+        }
+    }
+
+    public List<ItemDTO> getItems() {
+        return items;
+    }
+}
--- src/main/java/servlet/StandServlet.java
+++ src/main/java/servlet/StandServlet.java
@@ -1,30 +1,31 @@
-//package servlet;
-//
-//import dto.ItemDTO;
-////import service.Consumer;
-//
-//import javax.inject.Inject;
-//import javax.jms.ObjectMessage;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.util.List;
-//
-//@WebServlet(urlPatterns = { "/" })
-//public class StandServlet extends HttpServlet {
-//
-////    @Inject private Consumer consumer;
-//
-//    private String url = "/WEB-INF/views/main.xhtml";
-//
-//    @Override
-//    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-////        List<ItemDTO> receive = consumer.receive();
-////        resp.getWriter().write(String.valueOf(receive));
-//        resp.sendRedirect(url);
-////        req.getRequestDispatcher(url).forward(req, resp);
-//    }
-//}
+package servlet;
+
+import dto.ItemDTO;
+import service.Consumer;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(urlPatterns = { "/" })
+public class StandServlet extends HttpServlet {
+
+    @Inject private Consumer consumer;
+
+    private String url = "/WEB-INF/views/main.xhtml";
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        consumer.start();
+        resp.setContentType("text/html");
+        resp.setIntHeader("Refresh", 3);
+        PrintWriter writer = resp.getWriter();
+
+        writer.write(String.valueOf(consumer.getItems()));
+    }
+}
