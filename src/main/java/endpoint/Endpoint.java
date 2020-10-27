75e0a04899b45ff2a9c5eebf7aa78d6767255420
Paul Ponomarev <rocksquare@gmail.com>
2020-10-27 16:37:15 +0300

    PushEndpoint class refactored

--- src/main/java/endpoint/Endpoint.java
+++ src/main/java/endpoint/Endpoint.java
@@ -1,20 +1,41 @@
 package endpoint;
 
+import lombok.extern.java.Log;
+import org.primefaces.push.annotation.OnClose;
 import org.primefaces.push.annotation.OnMessage;
 import org.primefaces.push.annotation.OnOpen;
 import org.primefaces.push.annotation.PushEndpoint;
 import org.primefaces.push.impl.JSONEncoder;
 
+import javax.websocket.OnError;
+import java.util.logging.Level;
+
 @PushEndpoint("/push")
+@Log
 public class Endpoint {
 
     @OnMessage(encoders = {JSONEncoder.class})
-    public String onMessage(String count) {
-        return count;
+    public String onMessage(String message) {
+        System.err.println("Socket received message : " + message);
+        log.log(Level.INFO, "Socket received message : " + message);
+        return message;
     }
 
     @OnOpen
     public void onOpen() {
-        System.err.println("sock connected");
+        System.err.println("Server socket connected");
+        log.log(Level.INFO, "Server socket connected");
+    }
+
+    @OnClose
+    public void onClose() {
+        System.err.println("Server socket closed");
+        log.log(Level.INFO, "Server socket closed");
+    }
+
+    @OnError
+    public void onError(Exception e) {
+        System.err.println("Exception in Server Socket : " + e.getMessage());
+        log.log(Level.INFO, "Exception in Server Socket : " + e.getMessage());
     }
 }
